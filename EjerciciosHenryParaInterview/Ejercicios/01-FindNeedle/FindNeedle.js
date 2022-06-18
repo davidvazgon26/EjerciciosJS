a2455908c912c2437a1a60d80c492b867258fb06
davidvazgon26 <davidvazgon@hotmail.com>
2022-06-18 00:54:33 -0500

    resuelvo ejercicios 1 y 2 de Henry for Interview

--- EjerciciosHenryParaInterview/Ejercicios/01-FindNeedle/FindNeedle.js
+++ EjerciciosHenryParaInterview/Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,21 +1,40 @@
 function FindNeedle(haystack, needle) {
   // Your code here:
+  if(haystack.includes(needle)){
+    
+    for(let i=0;i<haystack.length;i++){
+        let cont = 1
+        if(haystack[i]===needle[0]){
+          for(j=1;j<needle.length;j++){
+            if(haystack[i+j]===needle[j]){
+                cont+=1
+            }
+          }
+        }
+        if(cont === needle.length){ return i}
+      }
 
-  // for (let i = 0; i < haystack.length; i++) {
-  //   for (let j = 0; j < needle.length; j++) {
-  //     if(haystack[i+j]!== needle[j]) break;
-  //     if (j+1 === needle.length) return i
-  //   }
-  // }
-  // return -1
+    }else{ return -1}
 
-  //complejidad temporal O(n*m)
-  //complejidad espacial O(1)
 
-  for (let i = 0; i < haystack.length; i++) {
-      if(haystack.slice(i,i+needle.length)===needle)  return i  
-  }
-return -1
+
 }
 
 module.exports = FindNeedle;
+
+//Aproximacion
+
+// recibo: string en haystack, palabra a encontrar en needle
+
+//ambos argumentos son string
+
+// validar si existe la palabra dada en el segundo argumento dentro del string, si no es asi devolver -1
+
+// Si existe debemos devolver el indice de la primera letra de la palabra buscada despues de recorrer el string
+
+//Para no hacer el recorrido de los 2 loop completos vamos validando que encuentre la primera letra, hasta entonces activamos el segundo loop
+
+//llevar un contador para detener el segundo loop cuando tenga el mismo tamanio que la palabra buscada
+
+//No usar indexOf()
+
--- EjerciciosHenryParaInterview/Ejercicios/02-SumArray/SumArray.js
+++ EjerciciosHenryParaInterview/Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,33 @@
 function SumArray(arr, n) {
   // Your code here:
 
+  if(arr.includes(n)) return true
+
+  while(arr.length > 0) {
+    let valor = arr.shift()
+    for(let i=0;i<arr.length;i++) {
+        if(valor+arr[i]===n){
+          return true
+        }
+    }
+  }
+  return false
 }
 
 module.exports = SumArray;
+
+//Aproximacion:
+
+//Recibo 2 argumentos, un arreglo y un numero
+
+//Validar si alguna combinacion de 2 numeros del arreglo suma la cantidad del numero pasado en el segundo argumento.
+
+//Si el valor se encuenta en el arreglo ya no sumo.
+
+// Tomo el primero valor y recorro el arreglo sumando y comparando con el resto hasta hacer todas las comparaciones posibles.
+
+//Al momento de encontrar una coincidencia detengo los loops
+
+//Si alguno de los numeros es mayor que el buscado lo salto
+
+//Si hay coincidencia devuelvo true, sino false
