aaaaca101d41bf8e52fd25c4a0a430ecc0edc557
davidvazgon26 <davidvazgon@hotmail.com>
2022-06-22 00:59:53 -0500

    ejercicio 10 de henry

--- EjerciciosHenryParaInterview/Ejercicios/10-DecimalToBynary/DecimalToBynary.js
+++ EjerciciosHenryParaInterview/Ejercicios/10-DecimalToBynary/DecimalToBynary.js
@@ -1,5 +1,14 @@
 function DecimalToBynary(num) {
   // Your code here:
+  let arr = []
+  while(num>0){
+
+    arr.push(num%2);
+    num = Math.floor(num/2);
+        
+  }
+  
+   return arr.reverse().join("")
 
 }
 
