b6bc134dd4efde3c07943767d1f5f9b3fa11a488
davidvazgon26 <davidvazgon@hotmail.com>
2022-06-22 00:43:29 -0500

    ejercicio 09 de henry

--- EjerciciosHenryParaInterview/Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
+++ EjerciciosHenryParaInterview/Ejercicios/09-BinaryToDecimal/BinaryToDecimal.js
@@ -1,6 +1,16 @@
 function BinaryToDecimal(binary) {
   // Your code here:
+  let potencia = 1
+  let arreglo = binary.split('');
+  let total = 0
 
+  for(let i=arreglo.length-1;i>=0;i--) {
+    total += potencia*parseInt(arreglo[i]);
+    potencia = potencia === 1? 2: potencia*2
 }
+return total
+}
+
+//console.log(BinaryToDecimal("11010"))
 
 module.exports = BinaryToDecimal;
