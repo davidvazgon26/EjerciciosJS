6ef49c1ef5683ff97e66c8a6e2a68f202c1a956b
davidvazgon26 <davidvazgon@hotmail.com>
2022-06-22 00:21:20 -0500

    ejercicios 07 y 08 de henry

--- EjerciciosHenryParaInterview/Ejercicios/07-SolveGraph/SolveGraph.js
+++ EjerciciosHenryParaInterview/Ejercicios/07-SolveGraph/SolveGraph.js
@@ -1,6 +1,18 @@
-function SolveGraph(graph, start, end) {
+function SolveGraph(graph, start, end, visited = {}) {
   // Your code here:
-
+  // nos fijamos si ya visitamos este vértice
+  if (visited[start]) return false;
+  // lo agregamos a los visitados
+  visited[start] = true;
+  // iteramos sobre el arreglo de vértices
+  for (let i = 0; i < graph[start].length; i += 1) {
+      // Si encontramos el target devolvemos true
+      if (end === graph[start][i]) return true;
+      // si no llamamos recursivamente a la función para ver si sus aristas están conectadas
+      else if (SolveGraph(graph, graph[start][i], end, visited)) return true
+}
+// si no devolvemos false
+return false;
 }
 
 module.exports = SolveGraph;
--- EjerciciosHenryParaInterview/Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
+++ EjerciciosHenryParaInterview/Ejercicios/08-FindWordStartingWith/FindWordStartingWith.js
@@ -1,5 +1,34 @@
 function FindWordStartingWith(book, query) {
   // Your code here:
+      
+      let arreglo = book.text.split('')
+      let buscado =query.split('')
+      let bandera = false
+      let posiciones = []
+
+      for(let i=0;i<arreglo.length-buscado.length;i++) {
+        if(arreglo[i] === buscado[0]&& arreglo[i-1]===" "){
+          bandera = true
+          for(let j=1;j<buscado.length;j++) {
+            if(arreglo[j+i] !== buscado[j]){
+                 bandera = false;
+                 break;
+            }
+           
+          }
+          
+        }else{ bandera = false; continue}
+        if(bandera === true) {
+          posiciones.push(i)
+        }
+       
+      }
+
+      console.log(buscado)
+      console.log(arreglo)
+      console.log(arreglo.length)
+      console.log(posiciones)
+      return posiciones
 
 }
 
