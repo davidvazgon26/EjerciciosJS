4e825f6fbf93ece64def16bd50d30754a714bf40
davidvazgon26 <davidvazgon@hotmail.com>
2022-07-09 00:16:57 -0500

    mas ejempls de valores tipo Map

--- EjerciciosJMircha/tipoDatoMap.js
+++ EjerciciosJMircha/tipoDatoMap.js
@@ -3,8 +3,52 @@
 
 let mapa = new Map();
 
-mapa.set("Nombre","David");
+mapa.set("nombre","David");
 mapa.set("apellido", "Vazquez");
 mapa.set("edad", 35)
 
-console.log(mapa)
+console.log(mapa);
+console.log("El total de elementos es de: " + mapa.size);
+
+// Ver si existe la propiedad
+
+console.log("Existe apellido? "+ mapa.has("apellido"));
+console.log("Existe correo? "+  mapa.has("correo"));
+
+// Obtener el valor de una propiedad
+
+console.log("El valor de la propiedad nombre es: " + mapa.get("nombre"))
+
+//Sobreescribir valor de la propiedad
+
+mapa.set("nombre", "David A")
+console.log(mapa);
+
+//Eliminar propiedad
+
+mapa.delete("edad")
+console.log(mapa);
+
+//Iterar el objeto
+
+for(let [key,value] of mapa){
+    console.log(`Llave:${key}, Valor:${value}`)
+}
+
+//Otra manera de armar un map
+
+const mapa2 = new Map([
+    ["nombre", "otro"],
+    [19, "numero"],
+    [null, "nulo"],
+    ["edad", 18],
+])
+
+console.log(mapa2)
+
+const llavesMapa2 = [...mapa2.keys()]
+const valoresMapa2 = [...mapa2.values()]
+
+console.log(llavesMapa2)
+console.log(valoresMapa2)
+
