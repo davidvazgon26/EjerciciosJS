ab227858637053587e6702b40dcab414873a36dd
davidvazgon26 <davidvazgon@hotmail.com>
2022-07-08 23:59:37 -0500

    Trabajando con valores tipo Map

---
+++ EjerciciosJMircha/tipoDatoMap.js
@@ -0,0 +1,10 @@
+//Es parecido a los objetos, pero solo para su iteracion
+
+
+let mapa = new Map();
+
+mapa.set("Nombre","David");
+mapa.set("apellido", "Vazquez");
+mapa.set("edad", 35)
+
+console.log(mapa)
