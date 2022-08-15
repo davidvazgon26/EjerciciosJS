b606e084ecd00101165253605bce3facdec2c701
davidvazgon26 <davidvazgon@hotmail.com>
2022-08-15 12:37:25 -0500

    utilizando genericos en JAVA

---
+++ CursoPOO/Java/MetodoGenericoParaListas.java
@@ -0,0 +1,10 @@
+package CursoPOO.Java;
+
+public class MetodoGenericoParaListas {
+    
+    public static void main(String[] args) {
+        //creo 3 array de diferentes tipos
+
+        static Character[] charArray = {'h','e','l','l','o'};
+    }
+}
