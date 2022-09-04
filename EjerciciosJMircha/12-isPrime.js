7fd8da79dcb6940f2ffbf6cb015052420820d873
davidvazgon26 <davidvazgon@hotmail.com>
2022-09-04 00:06:13 -0500

    commit

--- EjerciciosJMircha/12-isPrime.js
+++ EjerciciosJMircha/12-isPrime.js
@@ -5,13 +5,18 @@ const isPrime = (n) => {
 
     let cont = 0
   
+
+
     if(n === undefined) { console.warn("debes ingresar algun numero"); return false}
     if(n === 0) {console.warn("El cero no esta definido, ingresa otro numero"); return false  }
     if(n<0){ console.warn('debes ingresar un numero positivo'); return false }
 
     for(let i= 1 ;i<=n;i++){
+
         if(n%i === 0){ cont += 1}
+        
     }
+
 
     if(cont === 2){ console.info(`El numero ${n} es primo`); return true}
     else{ console.info( n +" No es primo"); return false}
