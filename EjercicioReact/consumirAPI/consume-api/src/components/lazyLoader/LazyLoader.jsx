46d5f68f0b3df0d7a741a4b0f9b9e6f19f935214
davidvazgon26 <davidvazgon@hotmail.com>
2022-09-22 00:22:16 -0500

    cambios en ejercicio de lazy loader

--- EjercicioReact/consumirAPI/consume-api/src/components/lazyLoader/LazyLoader.jsx
+++ EjercicioReact/consumirAPI/consume-api/src/components/lazyLoader/LazyLoader.jsx
@@ -5,9 +5,12 @@ function LazyLoader (){
 
     return(
         <div>
-            <img src ={require("../../assets/images/logo192.png")} width="auto" height="250px"/>
+            <img src ={require("../../assets/images/logo192.png")} width="auto" height="250px" alt="imagen de carga retardada"/>
         </div>
     )
 }
 
 export default LazyLoader;
+
+
+//Como cargar solo los componentes o elementos necesarios.
