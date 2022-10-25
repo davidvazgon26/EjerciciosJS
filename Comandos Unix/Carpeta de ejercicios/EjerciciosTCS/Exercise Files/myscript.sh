766d15b6386202fe90e924acd04c44e162b69140
davidvazgon26 <davidvazgon@hotmail.com>
2022-10-24 22:21:43 -0500

    nuevos comandos de unix

---
+++ Comandos Unix/Carpeta de ejercicios/EjerciciosTCS/Exercise Files/myscript.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+#echo "Hello world"
+
+#This is a comment
+
+#echo "Bye"
+
+
+#Variables
+
+myvar=hello
+
+myvar2="Mi variable 2"
+
+varnumber=26
+
+echo $myvar
+
+
+echo $myvar2
+
+echo $varnumber
+
+
