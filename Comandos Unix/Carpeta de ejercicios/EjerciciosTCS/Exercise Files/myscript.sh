054934e1458e2fa2f46ff4b0373b7220e83006fe
davidvazgon26 <davidvazgon@hotmail.com>
2022-10-26 23:35:49 -0500

    script en unix

--- Comandos Unix/Carpeta de ejercicios/EjerciciosTCS/Exercise Files/myscript.sh
+++ Comandos Unix/Carpeta de ejercicios/EjerciciosTCS/Exercise Files/myscript.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 #echo "Hello world"
 
+
+
+lflf
 #This is a comment
 
 #echo "Bye"
@@ -16,9 +19,58 @@ varnumber=26
 
 echo $myvar
 
+myvar="cambio el texto"
+
+echo intento cambiar la variable myvar:  $myvar
 
 echo $myvar2
 
 echo $varnumber
 
+# Variables-constantes
 
+declare -r myname="David"
+
+echo $myname
+
+myname="No se puede cambiar"
+
+echo intento cambiar la variable myname: $myname
+
+# declare con -l por lowercase
+
+declare -l lowerstring="This is some TEXT!"
+
+echo "El valor de la variable lowerstring es: $lowerstring"
+
+lowerstring="Cambie el VALOR!"
+
+echo "Ahora el valor de la variable lowerstring es: $lowerstring"
+
+# decalre con -u para uppercase
+
+declare -u upperstring="This os some TEXT!!"
+
+echo "El valor de la variable upperstring es: $upperstring"
+
+upperstring="Estos CAMBIANDO el Valor!!!"
+
+echo "Ahora el valor de la variable upperstring es: $upperstring"
+
+echo "Id de proceso de shell = $$"
+
+echo "Nombre del programa = $0"
+
+echo "Otra vez el nombre del programa = 0"
+
+echo "Numero de argumentos $#"
+
+echo "Argumento 1 = $1"
+
+echo "Argumento 2 = $2"
+
+echo "Lista completa de argumentos $*"
+
+echo "Lista completa de argumentos $@"
+
+echo "Este que hace? $f"
