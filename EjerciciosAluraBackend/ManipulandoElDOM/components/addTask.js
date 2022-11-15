18d65a5b3565c17e2470c7005c457b733a40e2a5
davidvazgon26 <davidvazgon@hotmail.com>
2022-11-15 15:08:48 -0600

    finalizando To do agregando boton para eliminar y actualizacion de display y localstorage al hacer click en delete

--- EjerciciosAluraBackend/ManipulandoElDOM/components/addTask.js
+++ EjerciciosAluraBackend/ManipulandoElDOM/components/addTask.js
@@ -1,8 +1,11 @@
+import { uniqueDates } from '../services/date.js';
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTask.js';
 
 export const addTask = (evento) => {
   evento.preventDefault();
+
   const list = document.querySelector('[data-list]');
   const input = document.querySelector('[data-form-input]');
   const calendar = document.querySelector('[data-form-date]');
@@ -10,42 +13,62 @@ export const addTask = (evento) => {
   const value = input.value;
   const date = calendar.value;
   const dateFormat = moment(date).format('DD/MM/YYYY');
-  // console.log(dateFormat);
+
+  if(value === "" || date === "") {
+    alert('Aun faltan campos por llenar')
+    return
+  }
 
   input.value = '';
-  calendar.value = "";
+  calendar.value = '';
+
+  // para los checks
+  const complete = false
 
   const taskObj = {
     value,
     dateFormat,
+    complete,
+    id: uuid.v4()
   };
 
+  // inicializar tarjetas para evitar duplicidad al cargar nuevo elemento
+list.innerHTML = '';
   const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-  // console.log(taskList);
   taskList.push(taskObj);
   localStorage.setItem('tasks', JSON.stringify(taskList));
 
-  const task = createTask(taskObj);
-  list.appendChild(task);
+  displayTasks();
+
+  // const task = createTask(taskObj);
+  // list.appendChild(task);
 };
 
-export const createTask = ({ value, dateFormat }) => {
+export const createTask = ({ value, dateFormat, complete, id }) => {
   const task = document.createElement('li');
-    task.classList.add('card');
-  //backticks
+  task.classList.add('card');
+
   const taskContent = document.createElement('div');
 
+  const check = checkComplete(id)
+
+  if (complete) {
+    check.classList.toggle('fas');
+    check.classList.toggle('completeIcon');
+    check.classList.toggle('far');
+  }
+
   const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(check);
+  taskContent.appendChild(titleTask);
 
   const dateElement = document.createElement('span');
-    dateElement.innerHTML = dateFormat;
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
+  dateElement.innerHTML = dateFormat;
+
+  task.appendChild(taskContent);
+  task.appendChild(dateElement);
+  task.appendChild(deleteIcon(id));
   return task;
 };
--- EjerciciosAluraBackend/ManipulandoElDOM/components/deleteIcon.js
+++ EjerciciosAluraBackend/ManipulandoElDOM/components/deleteIcon.js
@@ -1,13 +1,28 @@
-const deleteIcon = () => {
+import { displayTasks } from "./readTask.js";
+
+const deleteIcon = (id) => {
   const i = document.createElement('i');
   i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-  i.addEventListener('click', deleteTask);
+  i.addEventListener('click', () =>  deleteTask(id));
   return i;
 };
 
-const deleteTask = (event) => {
-  const parent = event.target.parentElement;
-  parent.remove();
+const deleteTask = (id) => {
+  // const parent = event.target.parentElement;
+  // parent.remove();
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  // elimina la tarea del arreglo
+  const index = tasks.findIndex(item => item.id === id);
+  tasks.splice(index,1);
+  // actualizar la vista
+  // limpieza de cards
+  const li = document.querySelector("[data-list]");
+  li.innerHTML = "";
+  //actualizar el local storage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTasks();
+
+  
 };
 
 export default deleteIcon;
