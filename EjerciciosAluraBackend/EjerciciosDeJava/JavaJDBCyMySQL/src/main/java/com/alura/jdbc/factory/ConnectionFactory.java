aed5859d20c9e09ab8e727257b9496a5a7d5ff50
davidvazgon26 <davidvazgon@hotmail.com>
2023-02-04 21:24:53 -0600

    agregando c3p0 para administrar las conexiones a la base de datos y reconfigurando el connectionFactory

--- EjerciciosAluraBackend/EjerciciosDeJava/JavaJDBCyMySQL/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ EjerciciosAluraBackend/EjerciciosDeJava/JavaJDBCyMySQL/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,34 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+import javax.sql.DataSource;
 
-    public Connection recuperaConexion() throws SQLException{
-        return DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-				"dbuser",
-				"dbuser");
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+    // Utilizando un pool de dataset para administrar nuestras conexiones
+    private DataSource datasource;
+
+    public ConnectionFactory() {
+        var pooledDataSource = new ComboPooledDataSource();
+        pooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+        pooledDataSource.setUser("dbuser");
+        pooledDataSource.setPassword("dbuser");
+
+        this.datasource = pooledDataSource;
     }
+
+    public Connection recuperaConexion() throws SQLException {
+        return this.datasource.getConnection();
+    }
+    /**
+     * public Connection recuperaConexion() throws SQLException{
+     * return DriverManager.getConnection(
+     * "jdbc:mysql://localhost:3306/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+     * "dbuser",
+     * "dbuser");
+     * }
+     */
 }
