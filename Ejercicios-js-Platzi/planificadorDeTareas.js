b42872e03f294e6bc37794e5aaadaf01eb23e85b
davidvazgon26 <davidvazgon@hotmail.com>
2023-03-19 23:32:01 -0600

    finalizacion de ejercicio de planificador de tareas

--- Ejercicios-js-Platzi/planificadorDeTareas.js
+++ Ejercicios-js-Platzi/planificadorDeTareas.js
@@ -1,15 +1,15 @@
  /**
   En este desafío, debes implementar la lógica de un planificador de tareas que permita agregar, eliminar y marcar como completadas las tareas, así como también mostrar un registro de las mismas. Para ello, debes construir la lógica de la función closure llamada createTaskPlanner para que devuelva los siguientes métodos:
 
-addTask(task): recibe un objeto que contiene la tarea y la agrega al array de tareas. La tarea debe estar conformada por las siguientes propiedades: id, name, priority, tags y completed, donde el estado completed se agrega automáticamente como falso al momento de agregar una tarea.
-removeTask(value): recibe el id o nombre de la tarea y la elimina del array de tareas.
-getTasks(): Devuelve el array de tareas.
-getPendingTasks(): Devuelve solo las tareas pendientes.
-getCompletedTasks(): Devuelve solo las tareas completadas.
-markTaskAsCompleted(value): Recibe el id o nombre de la tarea y la marca como completada.
-getSortedTasksByPriority(): Devuelve una copia de las tareas ordenadas según su prioridad (3: poco urgente, 2: urgente, 1: muy urgente), sin modificar la lista de tareas original.
-filterTasksByTag(tag): Filtra las tareas por una etiqueta específica.
-updateTask(taskId, updates): Buscar la tarea correspondiente con el id especificado y actualizar sus propiedades con las especificadas en el objeto updates.
+**addTask(task): recibe un objeto que contiene la tarea y la agrega al array de tareas. La tarea debe estar conformada por las siguientes propiedades: id, name, priority, tags y completed, donde el estado completed se agrega automáticamente como falso al momento de agregar una tarea.
+**removeTask(value): recibe el id o nombre de la tarea y la elimina del array de tareas.
+**getTasks(): Devuelve el array de tareas.
+**getPendingTasks(): Devuelve solo las tareas pendientes.
+**getCompletedTasks(): Devuelve solo las tareas completadas.
+**markTaskAsCompleted(value): Recibe el id o nombre de la tarea y la marca como completada.
+**getSortedTasksByPriority(): Devuelve una copia de las tareas ordenadas según su prioridad (3: poco urgente, 2: urgente, 1: muy urgente), sin modificar la lista de tareas original.
+**filterTasksByTag(tag): Filtra las tareas por una etiqueta específica.
+**updateTask(taskId, updates): Buscar la tarea correspondiente con el id especificado y actualizar sus propiedades con las especificadas en el objeto updates.
   */
  
  function createTaskPlanner() {
@@ -46,8 +46,8 @@ updateTask(taskId, updates): Buscar la tarea correspondiente con el id especific
         }
     },
     getSortedTasksByPriority: ()=>{
-        const sortedtask = [...task].sort((a, b) => a.priority - b.priority);
-      return sortedtask;
+        const sortedTask = [...task].sort((a, b) => a.priority - b.priority);
+      return sortedTask;
     },
     filterTasksByTag:(tag) => {
         return task.filter((task) => task.tags.includes(tag));
