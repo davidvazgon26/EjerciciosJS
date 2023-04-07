c3278a0f50fff30867265bab30f60a9502a10abf
davidvazgon26 <davidvazgon@hotmail.com>
2023-04-06 23:44:33 -0600

    ejercicios de js con querySelector

--- EjerciciosJMircha/61-dom.js
+++ EjerciciosJMircha/61-dom.js
@@ -31,3 +31,31 @@ setTimeout(() => {
 }, 4000);
 
 document.write("<h3>HOla mundo desde el DOM</h3>")
+
+//    Clase 62 
+
+//Estos ya no suelen utilizarce:
+
+console.log(document.getElementsByTagName("li")) //Tratar de ya no usar
+
+console.log(document.getElementsByClassName("card")) //Tratar de ya no usar
+
+console.log(document.getElementsByName("nombre")) //Tratar de ya no usar
+
+console.log(document.getElementById("menu"))
+
+// reemplazados por :
+
+console.log(document.querySelector("#menu"))
+console.log(document.querySelector(".card"))
+console.log(document.querySelector("li"))
+console.log(document.querySelector("a"))
+console.log(document.querySelectorAll("a"))
+console.log(document.querySelectorAll("a").length)
+
+
+document.querySelectorAll("a").forEach(el => console.log(el))
+
+console.log(document.querySelectorAll(".card")[2])
+
+console.log(document.querySelectorAll("#menu li"))
