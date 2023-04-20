465fe4a5acf077c57c551fec18a46b6974f92e15
davidvazgon26 <davidvazgon@hotmail.com>
2023-04-19 23:58:24 -0600

    ejercicio de reloj digital con js y selectores multiples

--- EjerciciosJMircha/ejerciciosLeccion80/index.js
+++ EjerciciosJMircha/ejerciciosLeccion80/index.js
@@ -1,7 +1,9 @@
-import hamburgerMenu from "./menuHamburguesa.js";
+import hamburgerMenu from "./dom/menuHamburguesa.js";
+import { digitalClock, alarm } from "./dom/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e)=>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
 });
