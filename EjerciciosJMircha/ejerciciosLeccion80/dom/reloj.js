8a172445106bcc927d3371a2684fc62e844007c9
davidvazgon26 <davidvazgon@hotmail.com>
2023-04-20 00:21:57 -0600

    ejercicio de reproductor mp3 de alarma, setTimeout y clearTimeout

--- EjerciciosJMircha/ejerciciosLeccion80/dom/reloj.js
+++ EjerciciosJMircha/ejerciciosLeccion80/dom/reloj.js
@@ -20,4 +20,29 @@ export function digitalClock(clock, btnPlay, btnStop){
     })
 }
 
-export function alarm(){}
+export function alarmaSonora(sound, btnPlay, btnStop){
+    let alarmTempo;
+    const $alarm = d.createElement("audio");
+    $alarm.src = sound;
+
+   d.addEventListener("click", e=>{
+    
+    if (e.target.matches(btnPlay)) {
+        alarmTempo = setTimeout(() => {
+            $alarm.play();
+        },1000);
+
+        e.target.disabled = true
+    }
+
+    if (e.target.matches(btnStop)) {
+        clearTimeout(alarmTempo);
+        $alarm.pause();
+        $alarm.currentTime = 0;
+        d.querySelector(btnPlay).disabled = false;
+
+    }
+
+
+   })
+}
--- EjerciciosJMircha/ejerciciosLeccion80/index.js
+++ EjerciciosJMircha/ejerciciosLeccion80/index.js
@@ -1,9 +1,10 @@
 import hamburgerMenu from "./dom/menuHamburguesa.js";
-import { digitalClock, alarm } from "./dom/reloj.js";
+import { digitalClock, alarmaSonora } from "./dom/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e)=>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
+    alarmaSonora("./assets/alarma.mp3","#activar-alarma", "#desactivar-alarma")
 });
