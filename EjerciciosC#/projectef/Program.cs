f9da0140cc2480d3166ab4a9940a06927dedf266
davidvazgon26 <davidvazgon@hotmail.com>
2023-05-26 00:14:33 -0600

    creando modelos, conexion a base de datos en memoria y probando la conexion a db y respuesta de las rutas en api de C#

--- EjerciciosC#/projectef/Program.cs
+++ EjerciciosC#/projectef/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+// builder.Services.AddSqlServer<TareasContext>(p => p.UseInMemoryDatabase("Data Source= NombreDelServidor; Initial Catalog=NameDBDb; user id=sa;password=miPassword"));
+builder.Services.AddDbContext<TareasContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("cnTareas")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
