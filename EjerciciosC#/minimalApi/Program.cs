de2c76e1e9ab817f6c259399bfa7404deb7daf7f
davidvazgon26 <davidvazgon@hotmail.com>
2023-07-23 21:36:18 -0600

    documentacion en proyecto minimaml api

--- EjerciciosC#/minimalApi/Program.cs
+++ EjerciciosC#/minimalApi/Program.cs
@@ -4,6 +4,8 @@ var app = builder.Build();
 // Declaración e inicialización de un arreglo de enteros
 int[] numeros = { 1, 2, 3, 4, 5 };
 
+/*en minimal api es para microservicios ya que no esta disenado para proyectos grandes ya que todos las rutas se encontraran aqui, abajo unos ejemplos de su uso*/
+
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/api", () => "Hello api!");
