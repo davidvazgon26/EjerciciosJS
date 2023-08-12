db75af3a6505baf311c0c923ee4adb824f01e7fb
davidvazgon26 <davidvazgon@hotmail.com>
2023-08-12 01:01:29 -0600

    aprendiendo sobre el ciclo de vida del componente, Constructor, ngOnChange, OnInit, ngBefores, ngAfterInit

--- EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/img/img.component.ts
+++ EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/img/img.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss'],
 })
-export class ImgComponent {
+export class ImgComponent
+  implements OnChanges, OnInit, AfterViewInit, OnDestroy
+{
   @Input() img: string = 'valor init de img';
   @Output() loaded = new EventEmitter<String>();
   imageDefault =
@@ -15,6 +26,29 @@ export class ImgComponent {
     //before render
     // No correr cosas asincronas
     console.log('Constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges() {
+    //Before render
+    // Changes inputs -
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
+
+  ngOnInit() {
+    //Before render
+    // Aqui corremos cosas asincronas como fetch y solo corre una vez.
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
+
+  ngAfterViewInit() {
+    //after render
+    // handler children
+    console.log('AfterViewInit', 'imgValue =>', this.img);
+  }
+
+  ngOnDestroy(): void {
+    //Delete
+    console.log('ngOnDestroy');
   }
 
   imgError() {
