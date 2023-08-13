af7a3d1be5f3bcf35b30f55a53fcb50f8775cfe1
davidvazgon26 <davidvazgon@hotmail.com>
2023-08-12 23:28:58 -0600

    comunicacion entre componentes padre e hijo

--- EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/product/product.component.ts
+++ EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Product } from '../../models/product.model';
 
@@ -14,4 +14,10 @@ export class ProductComponent {
     image: '',
     price: 0,
   };
+
+  @Output() addedProduct = new EventEmitter<Product>();
+
+  onAddToCart() {
+    this.addedProduct.emit(this.product);
+  }
 }
--- EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/products/products.component.ts
+++ EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/products/products.component.ts
@@ -7,6 +7,8 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
+  myShoppingCart: Product[] = [];
+  totalShoppinCart = 0;
   products: Product[] = [
     {
       id: '1',
@@ -20,5 +22,28 @@ export class ProductsComponent {
       price: 365,
       image: './assets/images/bike.jpg',
     },
+    {
+      id: '3',
+      name: 'El Mejor juguete',
+      price: 165,
+      image: './assets/images/toy.jpg',
+    },
+    {
+      id: '4',
+      name: 'Bicicleta casi nueva',
+      price: 765,
+      image: './assets/images/bike.jpg',
+    },
   ];
+
+  constructor() {}
+
+  onAddToShoppingCart(product: Product) {
+    console.log(product);
+    this.myShoppingCart.push(product);
+    this.totalShoppinCart = this.myShoppingCart.reduce(
+      (sum, item) => sum + item.price,
+      0
+    );
+  }
 }
