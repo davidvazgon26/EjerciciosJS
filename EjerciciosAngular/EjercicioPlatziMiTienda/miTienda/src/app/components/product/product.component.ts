204c999b33f11ba56438167f6b9838532f03b64f
davidvazgon26 <davidvazgon@hotmail.com>
2023-08-13 15:08:03 -0600

    uso de Http client en Angular

--- EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/product/product.component.ts
+++ EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/product/product.component.ts
@@ -10,9 +10,11 @@ import { Product } from '../../models/product.model';
 export class ProductComponent {
   @Input() product: Product = {
     id: '',
-    name: '',
+    title: '',
     image: '',
     price: 0,
+    description: '',
+    category: '',
   };
 
   @Output() addedProduct = new EventEmitter<Product>();
--- EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/products/products.component.ts
+++ EjerciciosAngular/EjercicioPlatziMiTienda/miTienda/src/app/components/products/products.component.ts
@@ -1,46 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
+//Servicios
 import { StoreService } from 'src/app/services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   myShoppingCart: Product[] = [];
   totalShoppinCart = 0;
-  products: Product[] = [
+  products: Product[] = [];
+
+  /*
+  products = [
     {
-      id: '1',
-      name: 'El Mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 365,
-      image: './assets/images/bike.jpg',
-    },
-    {
-      id: '3',
-      name: 'El Mejor juguete',
-      price: 165,
-      image: './assets/images/toy.jpg',
-    },
-    {
-      id: '4',
-      name: 'Bicicleta casi nueva',
-      price: 765,
-      image: './assets/images/bike.jpg',
-    },
-  ];
+        id: '1',
+        title: 'El Mejor juguete',
+        price: 565,
+        image: './assets/images/toy.jpg',
+        description: '',
+        category: '',
+      },
+      {
+        id: '2',
+        title: 'Bicicleta casi nueva',
+        price: 365,
+        image: './assets/images/bike.jpg',
+        description: '',
+        category: '',
+      },
+      {
+        id: '3',
+        title: 'El Mejor juguete',
+        price: 165,
+        image: './assets/images/toy.jpg',
+        description: '',
+        category: '',
+      },
+      {
+        id: '4',
+        title: 'Bicicleta casi nueva',
+        price: 765,
+        image: './assets/images/bike.jpg',
+        description: '',
+        category: '',
+      },
+  ]
+  */
 
   constructor(
-    private storeService: StoreService //Se inyecto el servicio
+    private storeService: StoreService, //Se inyecto el servicio
+    private productsService: ProductsService
   ) {
     this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    });
   }
 
   onAddToShoppingCart(product: Product) {
