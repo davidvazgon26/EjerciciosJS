eb15e3003500eb1f7d20e8139452c34f8226bbab
davidvazgon26 <davidvazgon@hotmail.com>
2023-08-27 22:18:43 -0600

    generando sesion, manejo de headers y utilizacion en componentes de datos devueltos como el profile del user

--- EjerciciosAngular/EjercicioPlatziApiAngular/angular-APIS-main/src/app/components/nav/nav.component.ts
+++ EjerciciosAngular/EjercicioPlatziApiAngular/angular-APIS-main/src/app/components/nav/nav.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from '../../services/store.service'
+import { StoreService } from '../../services/store.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user.models';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
   activeMenu = false;
   counter = 0;
+  token = '';
+  profile: User | null = null;
 
   constructor(
-    private storeService: StoreService
-  ) { }
+    private storeService: StoreService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.storeService.myCart$.subscribe(products => {
+    this.storeService.myCart$.subscribe((products) => {
       this.counter = products.length;
     });
   }
@@ -26,4 +31,23 @@ export class NavComponent implements OnInit {
     this.activeMenu = !this.activeMenu;
   }
 
+  login() {
+    this.authService
+      .login('david@ejemplo.com', '123456')
+      .pipe(
+        switchMap((token) => {
+          this.token = token.access_token;
+          return this.authService.profile(token.access_token);
+        })
+      )
+      .subscribe((user) => {
+        this.profile = user;
+      });
+  }
+
+  getProfile() {
+    this.authService.profile(this.token).subscribe((profile) => {
+      console.log(profile);
+    });
+  }
 }
--- EjerciciosAngular/EjercicioPlatziApiAngular/angular-APIS-main/src/app/services/auth.service.ts
+++ EjerciciosAngular/EjercicioPlatziApiAngular/angular-APIS-main/src/app/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from './../../environments/environment';
 import { Auth } from '../models/auth.model';
+import { User } from '../models/user.models';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,16 @@ export class AuthService {
     return this.http.post<Auth>(`${this.apiUrl}/login`, { email, password });
   }
 
-  profile() {
-    return this.http.get(`${this.apiUrl}/profile`);
+  profile(token: string) {
+    // const headers = new HttpHeaders();
+    // headers.set('Authorization', `Bearer ${token}`);
+    return this.http.get<User>(`${this.apiUrl}/profile`, {
+      // headers,
+      // sin metodo
+      headers: {
+        Authorization: `Bearer ${token}`,
+        // 'Content-type': 'application/json',
+      },
+    });
   }
 }
