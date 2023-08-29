0bf310705e5413cd2cea1d6d85847506b9b746dc
davidvazgon26 <davidvazgon@hotmail.com>
2023-08-28 21:13:04 -0600

    Creando contexto para el interceptor, cuando si o no usar los interceptors

--- EjerciciosAngular/EjercicioPlatziApiAngular/angular-APIS-main/src/app/services/products.service.ts
+++ EjerciciosAngular/EjercicioPlatziApiAngular/angular-APIS-main/src/app/services/products.service.ts
@@ -15,6 +15,7 @@ import {
 } from './../models/product.model';
 
 import { environment } from './../../environments/environment';
+import { checkTime } from '../interceptors/time.interceptor';
 
 @Injectable({
   providedIn: 'root',
@@ -33,17 +34,19 @@ export class ProductsService {
       params = params.set('offset', offset);
     }
 
-    return this.http.get<Product[]>(this.apiUrl, { params }).pipe(
-      retry(3),
-      map((products) =>
-        products.map((item) => {
-          return {
-            ...item,
-            taxes: 0.16 * item.price,
-          };
-        })
-      )
-    );
+    return this.http
+      .get<Product[]>(this.apiUrl, { params, context: checkTime() })
+      .pipe(
+        retry(3),
+        map((products) =>
+          products.map((item) => {
+            return {
+              ...item,
+              taxes: 0.16 * item.price,
+            };
+          })
+        )
+      );
   }
 
   getProduct(id: string) {
