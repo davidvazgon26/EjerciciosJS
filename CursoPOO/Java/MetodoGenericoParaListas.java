47c7a169a5332d413434803557660d119c113954
davidvazgon26 <davidvazgon@hotmail.com>
2023-09-09 20:53:56 -0600

    Reparando problema con clase de java

--- CursoPOO/Java/MetodoGenericoParaListas.java
+++ CursoPOO/Java/MetodoGenericoParaListas.java
@@ -1,10 +1,11 @@
 package CursoPOO.Java;
 
 public class MetodoGenericoParaListas {
-    
-    public static void main(String[] args) {
-        //creo 3 array de diferentes tipos
 
-        static Character[] charArray = {'h','e','l','l','o'};
+    public static void main(String[] args) {
+        // creo 3 array de diferentes tipos
+
+        Character[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+        System.out.println(charArray);
     }
 }
