2ec8053a53ce41fd1c1ccbe0565a7b350786abac
davidvazgon26 <davidvazgon@hotmail.com>
2023-09-11 23:13:05 -0600

    creando formulario de registro, categorias y validaciones de ambos

--- EjerciciosAngular/EjerciciosPlatziFormStore/src/app/admin/categories/components/category-form/category-form.component.ts
+++ EjerciciosAngular/EjerciciosPlatziFormStore/src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.scss']
+  styleUrls: ['./category-form.component.scss'],
 })
 export class CategoryFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.buildForm();
   }
 
+  ngOnInit(): void {}
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      image: ['', Validators.required],
+    });
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
 }
--- EjerciciosAngular/EjerciciosPlatziFormStore/src/app/auth/components/register/register.component.ts
+++ EjerciciosAngular/EjerciciosPlatziFormStore/src/app/auth/components/register/register.component.ts
@@ -34,16 +34,42 @@ export class RegisterComponent implements OnInit {
   }
 
   private buildForm() {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(6),
-          MyValidators.validPassword,
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            MyValidators.validPassword,
+          ],
         ],
-      ],
+        confirmPassword: ['', [Validators.required]],
+        type: ['company', [Validators.required]],
+        companyName: ['', [Validators.required]],
+      },
+      {
+        validators: MyValidators.matchPasswords,
+      }
+    );
+
+    this.typeField.valueChanges.subscribe((value) => {
+      //Cambios reactivos en las validaciones, en este ejemplo es con los radio-group
+      if (value === 'company') {
+        this.companyNameField.setValidators([Validators.required]);
+      } else {
+        this.companyNameField.setValidators(null);
+      }
+      this.companyNameField.updateValueAndValidity();
     });
+  }
+
+  get typeField() {
+    return this.form.get('type');
+  }
+
+  get companyNameField() {
+    return this.form.get('companyName');
   }
 }
