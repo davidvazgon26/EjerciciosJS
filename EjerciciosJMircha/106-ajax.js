3dccf43e216ba830e37e918e7b0c491dc39fa847
davidvazgon26 <davidvazgon@hotmail.com>
2023-10-10 22:11:30 -0600

    ejercicios de peticiones asincronas con AJAX y axios + async-await

--- EjerciciosJMircha/106-ajax.js
+++ EjerciciosJMircha/106-ajax.js
@@ -126,3 +126,31 @@
       console.log("Se ejecuta de cualquier forma para axios");
     });
 })();
+
+// Con Axios con async y await
+(() => {
+  const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+        json = await res.data;
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $axiosAsync.appendChild($fragment);
+    } catch (err) {
+      let message = err.response.statusText || "Ocurrio un error";
+      $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+    } finally {
+      console.log("De todos modos axion con async await");
+    }
+  }
+
+  getData();
+})();
