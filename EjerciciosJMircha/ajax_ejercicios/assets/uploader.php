12e108aa8176a6b5acb62221c6456da10f7f075d
davidvazgon26 <davidvazgon@hotmail.com>
2023-10-24 23:41:18 -0600

    ejercicio completo de carga de archivos con backend en php (recuerda correrlo dentro de xampp)

--- EjerciciosJMircha/ajax_ejercicios/assets/uploader.php
+++ EjerciciosJMircha/ajax_ejercicios/assets/uploader.php
@@ -8,5 +8,25 @@ if (isset($_FILES["file"])) {
     $file = $_FILES["file"]["tmp_name"]  ; 
     $error = $_FILES["file"]["error"]  ; 
     $destination = "../files/$name";
-    move_uploaded_file($file, $destination);
+    $upload=move_uploaded_file($file, $destination);
+
+    if ($upload) {
+        $res = array(
+            "err"=>false,
+            "status"=> http_response_code(200),
+            "statusText" => "Archivo $name subido con exito",
+            "files" => $_FILES["file"]
+        );
+    } else {
+        $res = array(
+            "err"=>true,
+            "status"=> http_response_code(400),
+            "statusText" => "Error al subir el archivo $name",
+            "files" => $_FILES["file"]
+        );
+    }
+    
+    echo json_encode($res);
 }
+
+?>
