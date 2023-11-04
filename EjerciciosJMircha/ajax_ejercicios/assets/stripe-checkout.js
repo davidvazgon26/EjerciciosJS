b45f4e7afff9dfd8c29c9bcaa420a6fe12488d97
davidvazgon26 <davidvazgon@hotmail.com>
2023-11-04 17:47:19 -0600

    fetch par aconexion a products y prices a manera de test, OK

--- EjerciciosJMircha/ajax_ejercicios/assets/stripe-checkout.js
+++ EjerciciosJMircha/ajax_ejercicios/assets/stripe-checkout.js
@@ -4,3 +4,29 @@ const d = document,
   $tacos = d.getElementById("tacos"),
   $template = d.getElementById("taco-template").content,
   $fragment = d.createDocumentFragment();
+
+fetch("https://api.stripe.com/v1/products", {
+  headers: {
+    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
+  },
+})
+  .then((res) => {
+    // console.log(res);
+    return res.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
+
+fetch("https://api.stripe.com/v1/prices", {
+  headers: {
+    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
+  },
+})
+  .then((res) => {
+    // console.log(res);
+    return res.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
