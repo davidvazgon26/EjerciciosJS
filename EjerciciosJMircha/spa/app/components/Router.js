647690f46efe55192546123b04ad8cb8fb776a08
davidvazgon26 <davidvazgon@hotmail.com>
2023-11-21 20:27:53 -0600

    configuracion del search con vanilla js

--- EjerciciosJMircha/spa/app/components/Router.js
+++ EjerciciosJMircha/spa/app/components/Router.js
@@ -25,7 +25,17 @@ export async function Router() {
       },
     });
   } else if (hash.includes("#/search")) {
-    $main.innerHTML = "<h2>Seccion del Buscador</h2>";
+    let query = localStorage.getItem("wpSearch");
+
+    if (!query) return false;
+
+    await ajax({
+      url: `${api.SEARCH}/${query}`,
+      cbSuccess: (search) => {
+        console.log(search);
+        // $main.innerHTML = Post(post);
+      },
+    });
   } else if (hash === "#/contacto") {
     $main.innerHTML = "<h2>Seccion de Contacto</h2>";
   } else {
