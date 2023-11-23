4cf8f21bdb4f77d3491b1fd8394d75859a59fcce
David Vazquez <73767511+davidvazgon26@users.noreply.github.com>
2023-11-22 19:12:54 -0600

    configuracion de postCards de Search y finalizacion del buscador

--- EjerciciosJMircha/spa/app/components/Router.js
+++ EjerciciosJMircha/spa/app/components/Router.js
@@ -2,6 +2,7 @@ import api from "../helpers/wp_api.js";
 import { ajax } from "../helpers/ajax.js";
 import { PostCard } from "./PostCard.js";
 import { Post } from "./Post.js";
+import {SearchCard} from "./SearchCard.js"
 
 export async function Router() {
   const d = document;
@@ -27,13 +28,28 @@ export async function Router() {
   } else if (hash.includes("#/search")) {
     let query = localStorage.getItem("wpSearch");
 
-    if (!query) return false;
+    if (!query) {
+      d.querySelector(".loader").style.display = "none";
+      return false;
+    }
 
     await ajax({
       url: `${api.SEARCH}/${query}`,
       cbSuccess: (search) => {
         console.log(search);
-        // $main.innerHTML = Post(post);
+        let html = "";
+
+        if (search.length === 0) {
+          html = `
+            <p class="error">
+             No existen resultados de busqueda para el temino
+             <mark> ${query}</mark> 
+            </p>
+            `;
+        } else {
+          search.forEach(post => html += SearchCard(post))
+        }
+         $main.innerHTML = html;
       },
     });
   } else if (hash === "#/contacto") {
