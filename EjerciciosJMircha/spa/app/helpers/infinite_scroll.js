2c7960b8ed64ee263f6de46947bc343597e62252
davidvazgon26 <davidvazgon@hotmail.com>
2023-11-24 23:39:20 -0600

    ejemplo de infinite scroll con vanilla js

--- EjerciciosJMircha/spa/app/helpers/infinite_scroll.js
+++ EjerciciosJMircha/spa/app/helpers/infinite_scroll.js
@@ -1,14 +1,47 @@
-export function InfiniteScroll() {
+import api from "./wp_api.js";
+import { ajax } from "./ajax.js";
+import { SearchCard } from "./../components/SearchCard.js";
+import { PostCard } from "./../components/PostCard.js";
+
+export async function InfiniteScroll() {
   const d = document,
     w = window;
 
   let query = localStorage.getItem("wpSearch"),
     apiURL,
     Component; //HOC  High Order Component
-  w.addEventListener("scroll", (e) => {
+
+  w.addEventListener("scroll", async (e) => {
     let { scrollTop, clientHeight, scrollHeight } = d.documentElement,
       { hash } = w.location;
 
     console.log(scrollTop, clientHeight, scrollHeight, hash);
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      api.page++;
+
+      if (!hash || hash === "#/") {
+        apiURL = `${api.POSTS}&page=${api.page}`;
+        Component = PostCard;
+      } else if (hash.includes("#/search")) {
+        apiURL = `${api.SEARCH}${query}&page=${api.page}`;
+        Component = SearchCard;
+      } else {
+        return false;
+      }
+
+      d.querySelector(".loader").style.display = "block";
+
+      await ajax({
+        url: apiURL,
+        cbSuccess: (posts) => {
+          // console.log(post);
+          let html = "";
+          posts.forEach((post) => (html += Component(post)));
+          d.getElementById("main").insertAdjacentHTML("beforeend", html);
+          d.querySelector(".loader").style.display = "none";
+        },
+      });
+    }
   });
 }
