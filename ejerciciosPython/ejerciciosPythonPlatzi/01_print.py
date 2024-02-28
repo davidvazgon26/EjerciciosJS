7c1ddf55ab0271b731c35c2522ae7659a5fa3017
davidvazgon26 <davidvazgon@hotmail.com>
2024-02-28 00:18:20 -0600

    ejercicios de python

---
+++ ejerciciosPython/ejerciciosPythonPlatzi/01_print.py
@@ -0,0 +1,21 @@
+print("Hola soy David")
+
+#operaciones
+
+print("suma: ", 12+5)
+print("resta: ", 10-5)
+print("multi: ", 2*3)
+print("div: ", 8/2)
+
+#Esto es un comentario
+
+"""
+varias 
+lineas
+otra
+"""
+
+'''
+varias
+lineas
+'''
---
+++ ejerciciosPython/ejerciciosPythonPlatzi/02_vars.py
@@ -0,0 +1,15 @@
+print ("Hola, vars")
+
+my_name = "David"
+
+print(my_name)
+
+my_age = 12
+print(my_age)
+
+my_name = "Alejandro"
+print(my_name)
+
+#input
+my_name = input("Cual es tu nombre?")
+print("usando input", my_name)
