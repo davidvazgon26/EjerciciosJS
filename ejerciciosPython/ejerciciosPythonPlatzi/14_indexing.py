c0a1c8d92f0ada7d7d401d862bea630c057e442e
davidvazgon26 <davidvazgon@hotmail.com>
2024-03-01 23:44:18 -0600

    ejercicios de areglos y recorridos con python

---
+++ ejerciciosPython/ejerciciosPythonPlatzi/14_indexing.py
@@ -0,0 +1,17 @@
+
+
+text = "Yo se Python"
+print(text)
+print(text[6])
+print(text[0:4])
+print(text[:8])
+print(text[2:-1])
+print(text[2:])
+print(text[:])
+print(text[0:7:2])
+print(text[::2])
+
+size = len(text)
+
+print(size)
+print(text[size-1])
---
+++ ejerciciosPython/ejerciciosPythonPlatzi/15_list.py
@@ -0,0 +1,56 @@
+numbers = [1,454,684,61,681,68,618]
+tasks = ['Make to do', "play something", "none", "string"]
+types = [1, True, "sfgfs", False, 14.55]
+
+print(type(numbers))
+print(numbers)
+
+print(type(tasks))
+print(tasks)
+
+print(type(types))
+print(types)
+
+print(numbers[3])
+print(tasks[3])
+
+print("do" in tasks)
+
+#CRUD
+
+numbers2 = [1,2,3,4,5,6,7,8]
+
+numbers2.append(700)
+
+print(numbers2)
+
+numbers2.insert(3,'Hola')
+
+print(numbers2)
+
+newList = numbers+ numbers2
+print(newList)
+
+index = newList.index('Hola')
+
+print(index)
+
+newList[index] = 'Adios'
+print(newList)
+
+newList.remove(681)
+print(newList)
+
+newList.pop()
+print(newList)
+
+newList.pop(1)
+print(newList)
+
+newList.reverse()
+print(newList)
+
+newList.pop(newList.index('Adios'))
+
+newList.sort()
+print(newList)
